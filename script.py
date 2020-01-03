b3bb6f287378653eb0f36006588f506174760e33
Christopher <chris@theantichris.com>
2020-01-03 11:35:33 -0500

    optimized sort

--- script.py
+++ script.py
@@ -4,10 +4,12 @@ def swap(arr, index_1, index_2):
     arr[index_2] = temp
 
 def bubble_sort(arr):
-    for el in arr:
-        for index in range(len(arr) - 1):
-            if arr[index] > arr[index + 1]:
-                swap(arr, index, index + 1)
+    for i in range(len(arr)):
+        # iterate through unplaced elements
+        for idx in range(len(arr) - i - 1):
+            if arr[idx] > arr[idx + 1]:
+                # swap elements
+                arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
     
 nums = [5, 2, 9, 1, 5, 6]
 print("Pre-Sort: {0}".format(nums))
