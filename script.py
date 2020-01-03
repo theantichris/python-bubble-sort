db14062a9a8aa7916804b6b00f818c7739c4d3c1
Christopher <chris@theantichris.com>
2020-01-03 11:25:27 -0500

    added method to perform the bubble sort

--- script.py
+++ script.py
@@ -3,6 +3,13 @@ def swap(arr, index_1, index_2):
     arr[index_1] = arr[index_2]
     arr[index_2] = temp
 
+def bubble_sort(arr):
+    for el in arr:
+        for index in range(len(arr) - 1):
+            if arr[index] > arr[index + 1]:
+                swap(arr, index, index + 1)
+    
 nums = [5, 2, 9, 1, 5, 6]
-swap(nums, 3, 5)
-print(nums)
+print("Pre-Sort: {0}".format(nums))
+bubble_sort(nums)
+print("Post-Sort: {0}".format(nums))
